e6d8a52a8e3c0b3fc995bbacd304ea265d57d1df
Julien Mourer <getkey@getkey.eu>
2021-03-22 22:47:37 +0100

    Add constructor typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,24 @@
+type Options = {
+	autoPlace?: boolean;
+	targetFPS?: number;
+	redrawInterval?: number;
+	maximumHistory?: number;
+	scale?: number;
+	memoryUpdateInterval?: number;
+	memoryMaxHistory?: number;
+	FONT_FAMILY?: string;
+	COLOR_FPS_BAR?: string;
+	COLOR_FPS_AVG?: string;
+	COLOR_TEXT_LABEL?: string;
+	COLOR_TEXT_TO_LOW?: string;
+	COLOR_TEXT_BAD?: string;
+	COLOR_TEXT_TARGET?: string;
+	COLOR_BG?: string;
+}
 declare class GameStats {
+	dom: HTMLCanvasElement;
+
+	constructor(options?: Options)
 	begin: (label?: string, color?: string) => void;
 	end: (label?: string, color?: string) => void;
 }
